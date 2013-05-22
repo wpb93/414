e008125990ebbda4fe87cdab4ec452f59487f5c7
Ryan HAN <hanzuohao123@gmail.com>
2013-05-22 21:24:22 +0800

    longin

--- csci4140/Models/AccountModels.cs
+++ csci4140/Models/AccountModels.cs
@@ -30,7 +30,7 @@ namespace csci4140.Models
     public class RegisterExternalLoginModel
     {
         [Required]
-        [Display(Name = "用户名")]
+        [Display(Name = "User Name")]
         public string UserName { get; set; }
 
         public string ExternalLoginData { get; set; }
@@ -40,51 +40,51 @@ namespace csci4140.Models
     {
         [Required]
         [DataType(DataType.Password)]
-        [Display(Name = "当前密码")]
+        [Display(Name = "Current Password")]
         public string OldPassword { get; set; }
 
         [Required]
         [StringLength(100, ErrorMessage = "{0} 必须至少包含 {2} 个字符。", MinimumLength = 6)]
         [DataType(DataType.Password)]
-        [Display(Name = "新密码")]
+        [Display(Name = "New Password")]
         public string NewPassword { get; set; }
 
         [DataType(DataType.Password)]
-        [Display(Name = "确认新密码")]
-        [Compare("NewPassword", ErrorMessage = "新密码和确认密码不匹配。")]
+        [Display(Name = "Confirm New Password")]
+        [Compare("NewPassword", ErrorMessage = "Not Match!")]
         public string ConfirmPassword { get; set; }
     }
 
     public class LoginModel
     {
         [Required]
-        [Display(Name = "用户名")]
+        [Display(Name = "User Name")]
         public string UserName { get; set; }
 
         [Required]
         [DataType(DataType.Password)]
-        [Display(Name = "密码")]
+        [Display(Name = "Password")]
         public string Password { get; set; }
 
-        [Display(Name = "记住我?")]
+        [Display(Name = "Remember Me?")]
         public bool RememberMe { get; set; }
     }
 
     public class RegisterModel
     {
         [Required]
-        [Display(Name = "用户名")]
+        [Display(Name = "User Name")]
         public string UserName { get; set; }
 
         [Required]
         [StringLength(100, ErrorMessage = "{0} 必须至少包含 {2} 个字符。", MinimumLength = 6)]
         [DataType(DataType.Password)]
-        [Display(Name = "密码")]
+        [Display(Name = "Password")]
         public string Password { get; set; }
 
         [DataType(DataType.Password)]
-        [Display(Name = "确认密码")]
-        [Compare("Password", ErrorMessage = "密码和确认密码不匹配。")]
+        [Display(Name = "Confirm Password")]
+        [Compare("Password", ErrorMessage = "Not Match!")]
         public string ConfirmPassword { get; set; }
     }
 
