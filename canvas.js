a477aaf421fc6689606663cd2923b55f42f47bb2
Wu Pangbo <i@wpb93.com>
2013-05-25 06:01:08 +0800

    Game fixed
    Vertical bounce checking has some bugs

--- canvas.js
+++ canvas.js
@@ -12,31 +12,25 @@ function Ball(position_x, position_y, speed_x, speed_y, ballRadius) {
     var myself = this;
     this.move = function () {
         if (myself.position.x > game.gameWidth - myself.radius || myself.position.x < myself.radius) {
-            myself.speed.x = myself.speed.x * -1;
+            myself.speed.x *= -1;
         }
 
 
         if (myself.position.y > game.gameHeight - myself.radius || myself.position.y < myself.radius) {
-            myself.speed.y = myself.speed.y * -1;
+            myself.speed.y *= -1;
         }
         
         for (var i = 0; i < game.lines.length; i++) {
-            if (game.lines[i].direction == Line.HORIZONTAL) {
-                var larger = game.lines[i].drawPoint.x >= game.lines[i].startPoint.x ? game.lines[i].drawPoint.x : game.lines[i].startPoint.x;
-                var smaller = game.lines[i].drawPoint.x >= game.lines[i].startPoint.x ? game.lines[i].startPoint.x : game.lines[i].drawPoint.x;
-                if (myself.position.y <= game.lines[i].startPoint.y + 4 && myself.position.y >= game.lines[i].startPoint.y - 4 && myself.position.x > smaller && myself.position.x < larger) {
-                    myself.speed.y = myself.speed.y * -1;
-                }
-            } else {
-                var larger = game.lines[i].drawPoint.y > game.lines[i].startPoint.y ? game.lines[i].drawPoint.y : game.lines[i].startPoint.y;
-                var smaller = game.lines[i].drawPoint.y > game.lines[i].startPoint.y ? game.lines[i].startPoint.y : game.lines[i].drawPoint.y;
-                if (myself.position.x <= game.lines[i].startPoint.x + 4 && myself.position.x >= game.lines[i].startPoint.x - 4 && myself.position.y > smaller && myself.position.y < larger) {
-                    myself.speed.x = myself.speed.x * -1;
+            if (game.lines[i].collide(myself)) {
+                if (game.lines[i].direction == Line.HORIZONTAL) {
+                    myself.speed.y *= -1;
+                } else {
+                    myself.speed.x *= -1;
                 }
             }
         }
-        myself.position.x = myself.position.x + parseFloat(myself.speed.x) / game.fps;
-        myself.position.y = myself.position.y + parseFloat(myself.speed.y) / game.fps;
+        myself.position.x += parseFloat(myself.speed.x) / game.fps;
+        myself.position.y += parseFloat(myself.speed.y) / game.fps;
     }
 }
 
@@ -55,19 +49,27 @@ function Line(start_x, start_y, end_x, end_y, drawSpeed) {
         //console.log(myself.drawPoint.y + " " + myself.endPoint.y);
         //console.log(" ");
 
-        if (myself.direction == 1) {
-            if (myself.drawPoint.x > myself.endPoint.x) {
-                myself.drawPoint.x -= parseFloat(myself.drawSpeed) / game.fps;
-            } else if (myself.drawPoint.x < myself.endPoint.x) {
-                myself.drawPoint.x += parseFloat(myself.drawSpeed) / game.fps;
-            }
-        } else {
-            if (myself.drawPoint.y > myself.endPoint.y) {
-                myself.drawPoint.y -= parseFloat(myself.drawSpeed) / game.fps;
-            } else if (myself.drawPoint.y < myself.endPoint.y) {
-                myself.drawPoint.y += parseFloat(myself.drawSpeed) / game.fps;
-            }
+        var drawDir = myself.direction == Line.HORIZONTAL ? 'x' : 'y';
+        if (myself.drawPoint[drawDir] == myself.endPoint[drawDir]) return;
+        myself.drawPoint[drawDir] += ((myself.endPoint[drawDir] - myself.startPoint[drawDir] > 0) ? 1 : -1) * parseFloat(myself.drawSpeed) / game.fps;
+        if (myself.drawPoint[drawDir] < Math.min(myself.startPoint[drawDir], myself.endPoint[drawDir])
+                || myself.drawPoint[drawDir] > Math.max(myself.startPoint[drawDir], myself.endPoint[drawDir])) {
+            myself.drawPoint[drawDir] = myself.endPoint[drawDir];
         }
+    };
+
+    this.collide = function (ball) {
+        var halfLineWidth = parseFloat(game.lineWidth) / 2;
+        var lineDir = myself.direction == Line.HORIZONTAL ? 'x' : 'y';
+        var lineDirPerp = myself.direction == Line.HORIZONTAL ? 'y' : 'x';
+        return ((ball.position[lineDirPerp] + ball.radius >= myself.startPoint[lineDirPerp] - halfLineWidth
+                && ball.position[lineDirPerp] - ball.radius <= myself.startPoint[lineDirPerp] + halfLineWidth)
+                && (ball.position[lineDir] >= Math.min(myself.startPoint[lineDir], myself.drawPoint[lineDir])
+                && ball.position[lineDir] <= Math.max(myself.startPoint[lineDir], myself.drawPoint[lineDir])));
+    };
+
+    this.finished = function () {
+        return myself.drawPoint.x == myself.endPoint.x && myself.drawPoint.y == myself.endPoint.y;
     }
 }
 
@@ -88,7 +90,7 @@ function Node(leftPoint, rightPoint) {
 
 }
 
-function Game(myCanvas, height, width, ballNumber, ballSpeed, ballRadius, lineSpeed, fps) {
+function Game(myCanvas, height, width, ballNumber, ballSpeed, ballRadius, lineSpeed, lineWidth, fps) {
     this.myCanvas = myCanvas;
     this.gameHeight = height;
     this.gameWidth = width;
@@ -99,13 +101,14 @@ function Game(myCanvas, height, width, ballNumber, ballSpeed, ballRadius, lineSp
     this.ballRadius = ballRadius;
     this.lines = new Array();
     this.lineSpeed = lineSpeed;
+    this.lineWidth = lineWidth;
     this.fps = fps;
     var myself = this;
     this.myCanvas.setAttribute('width', width);
     this.myCanvas.setAttribute('height', height);
     this.begin = function () {
         //new balls
-        myself.gameBoard[0][0] = new Node(new Point(0, 0), new Point(myself.gameWidth, myself.gameHeight));
+        myself.gameBoard = [[new Node(new Point(0, 0), new Point(myself.gameWidth, myself.gameHeight))]];
         for (var i = 0; i < ballNumber; i++) {
             var x = Math.random() * (myself.gameWidth - myself.ballRadius * 2) + myself.ballRadius;
             var y = Math.random() * (myself.gameHeight - myself.ballRadius * 2) + myself.ballRadius;
@@ -118,8 +121,8 @@ function Game(myCanvas, height, width, ballNumber, ballSpeed, ballRadius, lineSp
         }
 
         //addeventlistener
-        myself.myCanvas.addEventListener("click", drawHorizontalLine, false);
-        myself.myCanvas.addEventListener("contextmenu", drawVerticalLine, false);
+        myself.myCanvas.addEventListener("click", drawNewLine, false);
+        myself.myCanvas.addEventListener("contextmenu", drawNewLine, false);
 
         //update();
         setInterval(update, 1000.0 / myself.fps);
@@ -128,99 +131,90 @@ function Game(myCanvas, height, width, ballNumber, ballSpeed, ballRadius, lineSp
     function getNode(x, y) {
         for (var i = 0; i < myself.gameBoard.length; i++) {
             for (var j = 0; j < myself.gameBoard[i].length; j++) {
-                if (x > myself.gameBoard[i][j].leftPoint.x && x < myself.gameBoard[i][j].rightPoint.x && y > myself.gameBoard[i][j].leftPoint.y && y < myself.gameBoard[i][j].rightPoint.y) {
+                if (x >= myself.gameBoard[i][j].leftPoint.x
+                        && x < myself.gameBoard[i][j].rightPoint.x
+                        && y >= myself.gameBoard[i][j].leftPoint.y
+                        && y < myself.gameBoard[i][j].rightPoint.y) {
                     return new Point(i, j);
                 }
             }
         }
     }
 
-    function drawHorizontalLine(e) {
-        e.stopPropagation();
+    function drawNewLine(e) {
         e.preventDefault();
-        var x = e.clientX - myself.myCanvas.offsetLeft;
-        var y = e.clientY - myself.myCanvas.offsetTop;
-        var point = getNode(x, y);
-        for (var node = myself.gameBoard[point.x][point.y]; node.neighbor[Node.LEFT]; node = node.neighbor[Node.LEFT]);
-        var left = node.leftPoint.x;
-        var leftNewline = new Line(x, y, left, y, myself.lineSpeed);
-        myself.lines.push(leftNewline);
-
-        for (node = myself.gameBoard[point.x][point.y]; node.neighbor[Node.RIGHT]; node = node.neighbor[Node.RIGHT]);
-        var right = node.rightPoint.x;
-        var rightNewline = new Line(x, y, right, y, myself.lineSpeed);
-        myself.lines.push(rightNewline);
-    }
-
-    function drawVerticalLine(e) {
         e.stopPropagation();
-        e.preventDefault();
-        var x = e.clientX - myself.myCanvas.offsetLeft;
-        var y = e.clientY - myself.myCanvas.offsetTop;
-        var point = getNode(x, y);
-        for (var node = myself.gameBoard[point.x][point.y]; node.neighbor[Node.UP]; node = node.neighbor[Node.UP]);
-        var up = node.leftPoint.y;
-        var upNewline = new Line(x, y, x, up, myself.lineSpeed);
-        myself.lines.push(upNewline);
+        var dirs = {
+            HORIZONTAL: { nodeDir: [ 'LEFT', 'RIGHT' ], lineDir: 'x' },
+            VERTICAL: { nodeDir: ['UP', 'DOWN'], lineDir: 'y' }
+        };
+        var drawDir;
+        var drawDirPerp;
+        switch (e.button) {
+            case 0:
+                drawDir = dirs.HORIZONTAL;
+                drawDirPerp = dirs.VERTICAL;
+                break;
+            case 2:
+                drawDir = dirs.VERTICAL;
+                drawDirPerp = dirs.HORIZONTAL;
+                break;
+            default:
+                return;
+        }
+        
+        var clickPos = new Point(e.offsetX, e.offsetY);
+        var nodePos = getNode(clickPos.x, clickPos.y);
+        var node;
+        var endPoint = new Point();
+        endPoint[drawDirPerp.lineDir] = clickPos[drawDirPerp.lineDir];
 
-        for (node = myself.gameBoard[point.x][point.y]; node.neighbor[Node.DOWN]; node = node.neighbor[Node.DOWN]);
-        var down = node.rightPoint.y;
-        var downNewline = new Line(x, y, x, down, myself.lineSpeed);
-        myself.lines.push(downNewline);
+        for (node = myself.gameBoard[nodePos.x][nodePos.y]; node.neighbor[Node[drawDir.nodeDir[0]]]; node = node.neighbor[Node[drawDir.nodeDir[0]]]);
+        endPoint[drawDir.lineDir] = node.leftPoint[drawDir.lineDir];
+        myself.lines.push(new Line(clickPos.x, clickPos.y, endPoint.x, endPoint.y, myself.lineSpeed));
+
+        for (node = myself.gameBoard[nodePos.x][nodePos.y]; node.neighbor[Node[drawDir.nodeDir[1]]]; node = node.neighbor[Node[drawDir.nodeDir[1]]]);
+        endPoint[drawDir.lineDir] = node.rightPoint[drawDir.lineDir];
+        myself.lines.push(new Line(clickPos.x, clickPos.y, endPoint.x, endPoint.y, myself.lineSpeed));
     }
 
     function update() {
         var c = document.getElementById("myCanvas");
         var ctx = c.getContext("2d");
         ctx.clearRect(0, 0, myself.gameWidth, myself.gameHeight);
-        for(var i = 0; i < myself.balls.length; i++) {
+        ctx.fillStyle = "#FF0000";
+        ctx.beginPath();
+        for (var i = 0; i < myself.balls.length; i++) {
             myself.balls[i].move();
-            ctx.fillStyle = "#FF0000";
-            ctx.beginPath();
             ctx.arc(myself.balls[i].position.x, myself.balls[i].position.y, myself.balls[i].radius, 0, Math.PI * 2, true);
             ctx.closePath();
-            ctx.fill();
         }
+        ctx.fill();
+
+        ctx.beginPath();
+        ctx.lineWidth = myself.lineWidth;
         for (i = 0; i < myself.lines.length; i++) {
-            if (myself.lines[i].endPoint.x < myself.lines[i].drawPoint.x - 2 || myself.lines[i].endPoint.x > myself.lines[i].drawPoint.x + 2 || myself.lines[i].endPoint.y < myself.lines[i].drawPoint.y - 2 || myself.lines[i].endPoint.y > myself.lines[i].drawPoint.y + 2) {
-                var ifHit = false;
-                for (var j = 0; j < myself.balls.length; j++) {
-                    if (myself.lines[i].direction == Line.HORIZONTAL) {
-                        var larger = myself.lines[i].drawPoint.x >= myself.lines[i].startPoint.x ? myself.lines[i].drawPoint.x : myself.lines[i].startPoint.x;
-                        var smaller = myself.lines[i].drawPoint.x >= myself.lines[i].startPoint.x ? myself.lines[i].startPoint.x : myself.lines[i].drawPoint.x;
-                        if (myself.balls[j].position.y <= myself.lines[i].startPoint.y + 4 && myself.balls[j].position.y >= myself.lines[i].startPoint.y - 4 && myself.balls[j].position.x > smaller && myself.balls[j].position.x < larger) {
-                            ifHit = true;
-                        }
-                    } else {
-                        var larger = game.lines[i].drawPoint.y > game.lines[i].startPoint.y ? game.lines[i].drawPoint.y : game.lines[i].startPoint.y;
-                        var smaller = game.lines[i].drawPoint.y > game.lines[i].startPoint.y ? game.lines[i].startPoint.y : game.lines[i].drawPoint.y;
-                        if (myself.balls[j].position.x <= myself.lines[i].startPoint.x + 4 && myself.balls[j].position.x >= myself.lines[i].startPoint.x - 4 && myself.balls[j].position.y > smaller && myself.balls[j].position.y < larger) {
-                            ifHit = true;
-                        }
-                    }
-                    if (ifHit) {
-                        myself.lines.splice(i, 1);
-                        break;
-                    }
-                }
-                if (ifHit) {
-                    continue;
-                } else {
-                    myself.lines[i].draw();
+            myself.lines[i].draw();
+            var collide = false;
+            for (var j = 0; j < myself.balls.length; j++) {
+                if (!myself.lines[i].finished() && myself.lines[i].collide(myself.balls[j])) {
+                    myself.lines.splice(i--, 1);
+                    collide = true;
+                    break;
                 }
             }
-            
-            ctx.moveTo(myself.lines[i].startPoint.x, myself.lines[i].startPoint.y);
-            ctx.lineTo(myself.lines[i].drawPoint.x,myself.lines[i].drawPoint.y);
-            ctx.lineWidth = 2;
-            ctx.stroke();
+            if (!collide) {
+                ctx.moveTo(myself.lines[i].startPoint.x, myself.lines[i].startPoint.y);
+                ctx.lineTo(myself.lines[i].drawPoint.x, myself.lines[i].drawPoint.y);
+            }
         }
+        ctx.stroke();
     }
 }
 
 function init() {
     var myCanvas = document.getElementById("myCanvas");
-    game = new Game(myCanvas, 700, 600, 10, 400, 4, 200, 30);
+    game = new Game(myCanvas, 700, 600, 10, 400, 4, 200, 2, 60);
     game.begin();
 }
 
