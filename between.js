aa726387d9554c6924510c27718cbdc31c24cf08
yxwang2 <jimmerwearpurple@gmail.com>
2013-05-25 13:13:27 +0800

    new between.js

--- between.js
+++ between.js
@@ -1,13 +1,19 @@
 /* 
- * order:: a func that takes 2 parameters, return an integer > 0 if the first element is larger than the second element
+ * order:: a func that takes 2 parameters, return an integer > 0 if the first element is larger than the second element, else return 0
  * between:: return 1 if c is between a and b, else return 0 
+ * inclusive:: boolean 
  */
- function between(a,b,c,order) {
+ function between(a,b,c,inclusive,order) {
 	if (order) {
-		return (((order(a)>order(c)) && (order(c)>order(b))) || ((order(a)<order(c))&&(order(c)<order(b))));
+		return ((order(c,a) && order(b,c)) || (order(a,c) && order(c,b)));
 	}
 	else {
-		return (((a>c) && (c>b)) || ((a<c)&&(c<b)));
+		if (inclusive == 0) {
+			return (((a>=c) && (c>=b)) || ((a<=c) && (c<=b)));
+		}
+		else {
+			return (((a>c) && (c>b)) || ((a<c)&&(c<b)));
+		}
 	}
 }
 
